d8be62beb1f4a98deac40d014b165860c17aa6ed
T-McVee <iam@tmcvee.com>
2021-12-29 17:10:42 -0800

    start the skills component

---
+++ src/components/skills/Skills.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const Skills = () => {
+  return (
+    <div>
+      <h1>Skills</h1>
+      <FontAwesomeIcon icon={['fab', 'react']} />
+    </div>
+  );
+};
+
+export default Skills;
